9683013c52eaad37c724244d4ffebcf49fce5f0b
ThePotato10 <63891976+ThePotato10@users.noreply.github.com>
2025-02-04 13:28:27 +0000

    edits

--- MergeSort.java
+++ MergeSort.java
@@ -6,57 +6,61 @@ class MergeSort {
 
         for (int i = 0; i < (array.length / 2) - 1; i++) { // runs for the number of iterations necessary to sort fully
             int mergeSize = (int) Math.pow(2, i);
- 
-            int insertIndex = 0;
+
             for (int j = 0; j < array.length; j += (mergeSize * 2)) { // loop over all elements in array
+                if (i % 2 == 0) { // on even rounds, copy to aux array
+                    System.out.println("Indexes: " + Arrays.toString(new int[]{j, j + mergeSize - 1, j + mergeSize, j + (mergeSize * 2) - 1}));
 
-                for (int k = 0; k < mergeSize; k++) {
-                    int arrIndex = j + k;
+                    merge(j, j + mergeSize - 1, j + mergeSize, j + (mergeSize * 2) - 1, array, aux);
+                    System.out.println("Merge completed: " + Arrays.toString(aux));
+                } else {
+                    System.out.println("Indexes: " + Arrays.toString(new int[]{j, j + mergeSize - 1, j + mergeSize, j + (mergeSize * 2) - 1}));
 
-                    System.out.println("j: " + j);
-                    System.out.println("k: " + k);
-                    System.out.println("index 1: " + arrIndex);
-                    System.out.println("index 2: " + (arrIndex + mergeSize));
-                    System.out.println("Insert index: " + insertIndex);
-                    System.out.println("----------");
-
-                    if (i % 2 == 0) {// on even iterations, (incl 0) copy to aux array, on odds, copy to original array
-                        aux[insertIndex] = min(array[arrIndex], array[arrIndex + (mergeSize)]);
-                        aux[insertIndex + 1] = max(array[arrIndex], array[arrIndex + (mergeSize)]);
-
-                        System.out.println("aux: " + Arrays.toString(aux));
-                    } else {
-                        array[insertIndex] = min(aux[arrIndex], aux[arrIndex + (mergeSize)]);
-                        array[insertIndex + 1] = max(aux[arrIndex], aux[arrIndex + (mergeSize)]);
-
-                        System.out.println("array: " + Arrays.toString(array));
-                    }
-
-                    insertIndex += 2;
-                    System.out.println("----------");
+                    merge(j, j + mergeSize - 1, j + mergeSize, j + (mergeSize * 2) - 1, aux, array);
+                    System.out.println("Merge completed: " + Arrays.toString(array));
                 }
+
             }
 
             System.out.println("merge completed");
         }
 
-        System.out.println("final aux: " + Arrays.toString(aux));
-        System.out.println("final array: " + Arrays.toString(array));
-
         return aux;
     }
 
-    public static int min(int i1, int i2) {
-        if (i1 < i2) return i1;
-        else return i2;
-    }
+    public static void merge(int leftStart, int leftEnd, int rightStart, int rightEnd, int[] arr, int[] sorted) {
+        int sortedIndex = leftStart;
 
-    public static int max(int i1, int i2) {
-        if (i1 > i2) return i1;
-        else return i2;
+        while (leftStart <= leftEnd && rightStart <= rightEnd) {
+            if (arr[leftStart] < arr[rightStart]) {
+                sorted[sortedIndex] = arr[leftStart];
+                leftStart++;
+                sortedIndex++;
+    
+            } else {
+                sorted[sortedIndex] = arr[rightStart];
+                rightStart++;
+                sortedIndex++;
+    
+            }
+        }
+    
+        if (leftStart <= leftEnd) {
+            for (int i = leftStart; i <= leftEnd; i++) {
+                sorted[sortedIndex] = arr[i];
+                sortedIndex++;
+            }
+        }
+    
+        if (rightStart <= rightEnd) {
+            for (int i = rightStart; i <= rightEnd; i++) {
+                sorted[sortedIndex] = arr[i];
+                sortedIndex++;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sort(new int[]{1, 4, 6, 2, 7, 3, 9, 8})));
+        System.out.println(Arrays.toString(sort(new int[]{4, 1, 6, 2, 7, 3, 9, 8})));
     }
 }
